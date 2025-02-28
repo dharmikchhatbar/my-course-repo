3b7d8cc8739aa59390c8711f1dee8918cc030a9c
Dharmik Chhatbar <dharmikchhatbar01@gmail.com>
2025-02-28 00:10:43 -0500

    Initial commit with Django, database, and Flask notes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,42 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 class LessonInline(admin.StackedInline):
+    """Allows Lessons to be edited inline within CourseAdmin."""
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.StackedInline):
+    """Allows Choices to be edited inline within QuestionAdmin."""
+    model = Choice
+    extra = 2
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    """Allows Questions to be edited inline within CourseAdmin."""
+    model = Question
+    extra = 2
+
 class CourseAdmin(admin.ModelAdmin):
+    """Admin configuration for Course model."""
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    """Admin configuration for Question model."""
+    inlines = [ChoiceInline]
+    list_display = ['question_text'] 
 
 class LessonAdmin(admin.ModelAdmin):
+    """Admin configuration for Lesson model."""
     list_display = ['title']
 
-
-# <HINT> Register Question and Choice models here
-
+# Register models with Django admin
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
